672c7dd37dfc4a7f8ee6e4e243606223dbf9c170
mohdazfar <azfar-faizan@hotmail.com>
2016-11-07 23:48:00 +0200

    New Commit
    Giving the method data directly from the code not reading from the file

--- SDP/WatsonCloud.py
+++ SDP/WatsonCloud.py
@@ -7,8 +7,7 @@ class WatsonCloud:
     username = '9a513c41-c7ef-4c4b-85fe-78dc3fc02818'
     paasword = '5GyKzkuu547q'
 
-    def getTradeOffAnalyticsResult(self):
+    def getTradeOffAnalyticsResult(self, data):
         tradeoff_analytics = TradeoffAnalyticsV1(username=self.username ,password= self.paasword)
-        jsonFile = open('DataFile.json', 'r')
-        problem_data=json.load(jsonFile)
+        problem_data=json.loads(data)
         return json.dumps(tradeoff_analytics.dilemmas(problem_data),indent=3)

9bcabe39b83217b714f3ae16b55f8e6610205ac3
Zerayaeqob Cherenet <zekeymen@gmail.com>
2016-11-10 21:01:59 +0200

    updated codes

---
+++ TravelApp/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='travleAPI',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
